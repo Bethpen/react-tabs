159958904fcafbbea97810f4f201719fb9e5100c
Bethpen <64626802+Bethpen@users.noreply.github.com>
2021-07-30 21:23:27 +0100

    Updates

---
+++ precache-manifest.557f000140bf8ab5615841afcabd8db1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54927d75f6564b617d6fd572f1a5d544",
+    "url": "/react-tabs/index.html"
+  },
+  {
+    "revision": "3fdbca67ff05a7838b13",
+    "url": "/react-tabs/static/css/main.fd50c553.chunk.css"
+  },
+  {
+    "revision": "1fd6ce2224cf0d523c9a",
+    "url": "/react-tabs/static/js/2.bcafc819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tabs/static/js/2.bcafc819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fdbca67ff05a7838b13",
+    "url": "/react-tabs/static/js/main.ceaed22d.chunk.js"
+  },
+  {
+    "revision": "96f582e532e180acd4ac",
+    "url": "/react-tabs/static/js/runtime-main.45082ee9.js"
+  }
+]);
